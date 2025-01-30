63170bdbc790524bb8c9f1cd20a9c712658afc2e
Sarulatha28 <sarulathav05@gmail.com>
2025-01-30 13:28:14 +0530

    updated landing.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Landing from "./pages/Landing"
+import Landing from "./pages/Landing";
 
 const App = () => {
-
   return (
     <Router>
-      <div className="flex bg-red-100 min-h-screen">
+      <div >
         <Landing />
       </div>
     </Router>
